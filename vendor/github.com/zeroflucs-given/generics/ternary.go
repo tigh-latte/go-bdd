d751d8faad5b2f1468ec872bae2f55918925b958
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-06-07 10:23:32 +0100

    remove vendor

--- vendor/github.com/zeroflucs-given/generics/ternary.go
+++
@@ -1,9 +0,0 @@
-package generics
-
-// If returns the equivilent value based on the predicate
-func If[T any](condition bool, ifTrue, ifFalse T) T {
-	if condition {
-		return ifTrue
-	}
-	return ifFalse
-}
